aee6c9c89621f5a19de596ff3d92370ec8c2add0
MJay <01087153603a@gmail.com>
2024-08-25 15:33:13 +0900

    0825

---
+++ src/main/java/idusw/soccerworld/SoccerWorldApplication.java
@@ -0,0 +1,13 @@
+package idusw.soccerworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/idusw/soccerworld/SoccerWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package idusw.soccerworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
