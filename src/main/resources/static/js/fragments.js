d3ec13d7087535210fb71e18b39187d9a1ab1c02
MJay <01087153603a@gmail.com>
2024-10-07 16:02:55 +0900

    top-bar update
    1007

--- src/main/resources/static/js/fragments.js
+++ src/main/resources/static/js/fragments.js
@@ -1,15 +1,22 @@
+let leagueList = document.getElementById('leagueList');
 let teamList = document.getElementById('teamList');
 let eplList = document.getElementById('eplList');
 let laLigaList = document.getElementById('laLigaList');
 let serieAList = document.getElementById('serieAList');
 let bundesligaList = document.getElementById('bundesligaList');
+let showTypes = false;
 
 document.addEventListener('DOMContentLoaded', function() {
+    hideAll();
+});
+
+function hideAll() {
+    leagueList.style.display = 'none';
     eplList.style.display = 'none';
     laLigaList.style.display = 'none';
     serieAList.style.display = 'none';
     bundesligaList.style.display = 'none';
-});
+}
 
 function showTeams(name) {
     if(name == 'EPL') {
@@ -33,5 +40,15 @@ function showTeams(name) {
         eplList.style.display = 'none';
         laLigaList.style.display = 'none';
         serieAList.style.display = 'none';
+    }
+}
+
+function showLeagues() {
+    if(!showTypes) {
+        leagueList.style.display = 'table';
+        showTypes = true;
+    } else {
+        hideAll();
+        showTypes = false;
     }
 }
