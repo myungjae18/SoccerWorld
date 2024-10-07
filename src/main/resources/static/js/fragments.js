f313fc6d8e22a36f7e26945e7cd39ffa34782888
MJay <01087153603a@gmail.com>
2024-10-07 16:30:19 +0900

    top-bar update2
    1007 2

--- src/main/resources/static/js/fragments.js
+++ src/main/resources/static/js/fragments.js
@@ -1,21 +1,26 @@
 let leagueList = document.getElementById('leagueList');
+let leagueScheduleList = document.getElementById('leagueScheduleList');
 let teamList = document.getElementById('teamList');
 let eplList = document.getElementById('eplList');
 let laLigaList = document.getElementById('laLigaList');
 let serieAList = document.getElementById('serieAList');
 let bundesligaList = document.getElementById('bundesligaList');
-let showTypes = false;
+let isTeamInfoVisible = false;
+let isLeagueScheduleVisible = false;
 
 document.addEventListener('DOMContentLoaded', function() {
     hideAll();
 });
 
 function hideAll() {
+    //팀정보 테이블 감춤
     leagueList.style.display = 'none';
     eplList.style.display = 'none';
     laLigaList.style.display = 'none';
     serieAList.style.display = 'none';
     bundesligaList.style.display = 'none';
+    //리그순위정보 테이블 감춤
+    leagueScheduleList.style.display = 'none';
 }
 
 function showTeams(name) {
@@ -44,11 +49,24 @@ function showTeams(name) {
 }
 
 function showLeagues() {
-    if(!showTypes) {
+    if(!isTeamInfoVisible) {
+        hideAll();
         leagueList.style.display = 'table';
-        showTypes = true;
+        isTeamInfoVisible = true;
+    }
+    else {
+        hideAll();
+        isTeamInfoVisible = false;
+    }
+}
+
+function showLeagueSchedules() {
+    if(!isLeagueScheduleVisible) {
+        hideAll();
+        leagueScheduleList.style.display = 'table';
+        isLeagueScheduleVisible = true;
     } else {
         hideAll();
-        showTypes = false;
+        isLeagueScheduleVisible = false;
     }
 }
