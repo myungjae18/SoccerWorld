8d40df995ad195da9c6aa57ba19a4ab59bd310f0
MJay <01087153603a@gmail.com>
2024-10-09 18:12:26 +0900

    mybatis update
    1009

--- src/main/java/idusw/soccerworld/controller/MainController.java
+++ src/main/java/idusw/soccerworld/controller/MainController.java
@@ -1,5 +1,6 @@
 package idusw.soccerworld.controller;
 
+import idusw.soccerworld.repository.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class MainController {
+    final CategoryRepository categoryRepository;
+
+    public MainController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/index")
     public String goIndex() {
+        System.out.println(categoryRepository.selectAll());
         return "/main/index";
     }
 
---
+++ src/main/java/idusw/soccerworld/domain/entity/Category.java
@@ -0,0 +1,9 @@
+package idusw.soccerworld.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private Long category_id;
+    private String name;
+}
